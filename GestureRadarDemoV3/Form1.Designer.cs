0d3915417d6d8ed670370789ca5a8cd4790e3e48
frank.xiong <frank.xiong@timevary.com>
2020-02-21 14:04:20 +0800

    C# example

---
+++ GestureRadarDemoV3/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace GestureRadarDemoV3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroProgressBarZ = new MetroFramework.Controls.MetroProgressBar();
+            this.metroLabelZ = new MetroFramework.Controls.MetroLabel();
+            this.picBoxYZ = new System.Windows.Forms.PictureBox();
+            this.picBoxXY = new System.Windows.Forms.PictureBox();
+            this.metroTextBoxMsg = new MetroFramework.Controls.MetroTextBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxYZ)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxXY)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // metroProgressBarZ
+            // 
+            this.metroProgressBarZ.Location = new System.Drawing.Point(25, 792);
+            this.metroProgressBarZ.Maximum = 255;
+            this.metroProgressBarZ.Name = "metroProgressBarZ";
+            this.metroProgressBarZ.Size = new System.Drawing.Size(711, 40);
+            this.metroProgressBarZ.TabIndex = 2;
+            // 
+            // metroLabelZ
+            // 
+            this.metroLabelZ.AutoSize = true;
+            this.metroLabelZ.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabelZ.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.metroLabelZ.Location = new System.Drawing.Point(819, 792);
+            this.metroLabelZ.Name = "metroLabelZ";
+            this.metroLabelZ.Size = new System.Drawing.Size(23, 25);
+            this.metroLabelZ.TabIndex = 3;
+            this.metroLabelZ.Text = "0";
+            // 
+            // picBoxYZ
+            // 
+            this.picBoxYZ.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.picBoxYZ.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picBoxYZ.Location = new System.Drawing.Point(507, 1148);
+            this.picBoxYZ.Name = "picBoxYZ";
+            this.picBoxYZ.Size = new System.Drawing.Size(38, 0);
+            this.picBoxYZ.TabIndex = 1;
+            this.picBoxYZ.TabStop = false;
+            this.picBoxYZ.Visible = false;
+            // 
+            // picBoxXY
+            // 
+            this.picBoxXY.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picBoxXY.Location = new System.Drawing.Point(25, 98);
+            this.picBoxXY.Name = "picBoxXY";
+            this.picBoxXY.Size = new System.Drawing.Size(867, 653);
+            this.picBoxXY.TabIndex = 0;
+            this.picBoxXY.TabStop = false;
+            this.picBoxXY.Paint += new System.Windows.Forms.PaintEventHandler(this.picBoxXY_Paint);
+            // 
+            // metroTextBoxMsg
+            // 
+            this.metroTextBoxMsg.FontSize = MetroFramework.MetroTextBoxSize.Tall;
+            this.metroTextBoxMsg.FontWeight = MetroFramework.MetroTextBoxWeight.Bold;
+            this.metroTextBoxMsg.Location = new System.Drawing.Point(924, 98);
+            this.metroTextBoxMsg.Multiline = true;
+            this.metroTextBoxMsg.Name = "metroTextBoxMsg";
+            this.metroTextBoxMsg.Size = new System.Drawing.Size(286, 653);
+            this.metroTextBoxMsg.TabIndex = 5;
+            this.metroTextBoxMsg.Text = "Lrng";
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(924, 792);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(286, 40);
+            this.metroButton1.TabIndex = 6;
+            this.metroButton1.Text = "Send";
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1233, 862);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.metroTextBoxMsg);
+            this.Controls.Add(this.metroLabelZ);
+            this.Controls.Add(this.metroProgressBarZ);
+            this.Controls.Add(this.picBoxYZ);
+            this.Controls.Add(this.picBoxXY);
+            this.Name = "Form1";
+            this.Text = "GestureRadarDemoV3.2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxYZ)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxXY)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBoxXY;
+        private System.Windows.Forms.PictureBox picBoxYZ;
+        private MetroFramework.Controls.MetroProgressBar metroProgressBarZ;
+        private MetroFramework.Controls.MetroLabel metroLabelZ;
+        private MetroFramework.Controls.MetroTextBox metroTextBoxMsg;
+        private MetroFramework.Controls.MetroButton metroButton1;
+    }
+}
+
---
+++ GestureRadarDemoV3/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MetroFramework.Forms;
+
+namespace GestureRadarDemoV3
+{
+    public partial class Form1 : MetroForm
+    {
+        public class Point3D
+        {
+            public float x, y, z;
+            public Point3D(float x, float y, float z)
+            {
+                this.x = x;
+                this.y = y;
+                this.z = z;
+            }
+        };
+
+        private Size originalSize;
+        private Size originalXYSize;
+        private Size originalYZSize;
+
+        private int mAverageZ = 256;
+
+        private List<Point3D> path3DList;
+
+        //使用GestureRadarSDK进行开发，用C#调用C++的DLL读取串口
+        private delegate void GestureRadarSDKCallBack(float x, float y, float z);
+        private delegate void GestureRadarSDKSwipeCallBack(string strName);
+        private delegate void GestureRadarSDKTrailCallBack(string strName, double score, float minX, float maxX, float minY, float maxY, float z);
+
+
+        private static GestureRadarSDKCallBack callBackHandle;
+        private static GestureRadarSDKTrailCallBack trailSDKRecogniseHandle;
+        private static GestureRadarSDKSwipeCallBack swipeSDKRecogniseHandle;
+
+        [DllImport("GestureRadarSDK")]
+        private static extern void tvt_register_loop_callBack(bool bRecognizeSwipe,
+            bool bRecognizeTrail,
+            int nTemplateNum,
+            string[] strTemplateNames,
+            GestureRadarSDKCallBack callBack,
+            GestureRadarSDKSwipeCallBack swipeCallBack,
+            GestureRadarSDKTrailCallBack trailCallBack);
+
+        [DllImport("GestureRadarSDK")]
+        private static extern void tvt_release_loop_callBack();
+
+        [DllImport("GestureRadarSDK")]
+        public static extern bool tvt_write_raw_data(Byte[] rawData, int rawDataLen);
+
+        public delegate void SetPanelCallBack();
+
+        public void GestureRadarReceiveData(float x, float y, float z)
+        {
+            if (z > mAverageZ)
+            {
+                path3DList.Add(new Point3D(x, y, z));
+                if (path3DList.Count > 10)
+                {
+                    path3DList.RemoveAt(0);
+                }
+                return;
+            }
+            else
+            {
+                path3DList.Add(new Point3D(x, y, z));
+                if (path3DList.Count > 10)
+                {
+                    path3DList.RemoveAt(0);
+                }
+                picBoxXY.Invalidate();
+
+                SetPanelCallBack d = delegate
+                {
+                    metroProgressBarZ.Value = (int)z;
+                    metroLabelZ.Text = z.ToString();
+                };
+                metroProgressBarZ.Invoke(d);
+            }
+        }
+        public void GestureRadarTraildata(string strName, double score, float minX, float maxX, float minY, float maxY, float z)
+        {
+        }
+        public void GestureRadarSwipeData(string strName)
+        {
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            path3DList = new List<Point3D>();
+
+            callBackHandle = GestureRadarReceiveData;
+            trailSDKRecogniseHandle = GestureRadarTraildata;
+            swipeSDKRecogniseHandle = GestureRadarSwipeData;
+            tvt_register_loop_callBack(false, false, 0, null, callBackHandle, swipeSDKRecogniseHandle, trailSDKRecogniseHandle);
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.originalSize.Width == 0 || this.originalSize.Height == 0)
+            {
+                return;
+            }
+
+            float RadioW = (float)this.Size.Width / (float)originalSize.Width;
+
+            picBoxXY.Width = (int)(originalXYSize.Width * RadioW);
+            picBoxYZ.Width = (int)(originalYZSize.Width * RadioW);
+
+        }
+
+        private void picBoxXY_Paint(object sender, PaintEventArgs e)
+        {
+            SolidBrush brush = new SolidBrush(Color.Black);
+
+            float nResizeRadioW = (float)picBoxXY.Width / 80;
+            float nResizeRadioH = (float)picBoxXY.Height / 80;
+
+            for (int i = 0; i < path3DList.Count; i++)
+            {
+                Point3D point = path3DList[i];
+                brush.Color = Color.FromArgb(25 * i, Color.Black);
+                e.Graphics.FillEllipse(brush, (point.x + 40) * nResizeRadioW, (80 - (point.y + 40)) * nResizeRadioH, 10, 10);
+            }
+
+        }
+
+        public bool isShowParam = false;
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            if (isShowParam)
+            {
+                isShowParam = false;
+                this.Width -= 200;
+            }
+            else
+            {
+                isShowParam = true;
+                this.Width += 200;
+            }
+        }
+
+        private void metroButton1_Click_1(object sender, EventArgs e)
+        {
+            Console.WriteLine(metroTextBoxMsg.Text);
+            byte[] byteArray = System.Text.Encoding.Default.GetBytes(metroTextBoxMsg.Text);
+            byte[] sendArray = new byte[byteArray.Length + 2];
+            for (int i = 0; i < byteArray.Length; i++)
+            {
+                sendArray[i] = byteArray[i];
+            }
+            sendArray[sendArray.Length - 2] = 0x0D;
+            sendArray[sendArray.Length - 1] = 0x0A;
+            tvt_write_raw_data(sendArray, sendArray.Length);
+            metroTextBoxMsg.Clear();
+        }
+    }
+}
